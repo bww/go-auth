6f85f82b29bc611dfc0d1da252fd0384a02adcb6
Brian W. Wolter <brianwolter@gmail.com>
2025-02-06 10:40:59 -0500

    Adding more control over canonical requests

--- v1/httputil/request.go
+++ v1/httputil/request.go
@@ -9,11 +9,50 @@ import (
 
 const jwtParam = "jwt" // the JWT parameter; we must ignore this
 
-func CanonicalRequest(req *http.Request) string {
-	return CanonicalRequestComponents(req.Method, req.URL.Path, req.URL.Query())
+type CanonicalRequestConfig struct {
+	ExcludeParams []string
 }
 
-func CanonicalRequestComponents(method, resource string, params url.Values) string {
+func (c CanonicalRequestConfig) WithOptions(opts []CanonicalRequestOption) CanonicalRequestConfig {
+	for _, opt := range opts {
+		c = opt(c)
+	}
+	return c
+}
+
+func (c CanonicalRequestConfig) ExcludedParams() map[string]struct{} {
+	// If params are not defined, we use the default set, which is jus the JWT parameter itself
+	if c.ExcludeParams == nil {
+		return map[string]struct{}{jwtParam: {}}
+	}
+	x := make(map[string]struct{})
+	for _, e := range c.ExcludeParams {
+		x[e] = struct{}{}
+	}
+	return x
+}
+
+type CanonicalRequestOption func(CanonicalRequestConfig) CanonicalRequestConfig
+
+func WithExcludedParams(names ...string) CanonicalRequestOption {
+	return func(c CanonicalRequestConfig) CanonicalRequestConfig {
+		c.ExcludeParams = append(names, jwtParam) // always include the JWT param
+		return c
+	}
+}
+
+func CanonicalRequest(req *http.Request, opts ...CanonicalRequestOption) string {
+	return CanonicalRequestComponents(req.Method, req.URL.Path, req.URL.Query(), opts...)
+}
+
+func CanonicalRequestComponents(method, resource string, params url.Values, opts ...CanonicalRequestOption) string {
+	return CanonicalRequestComponentsWithConfig(method, resource, params, CanonicalRequestConfig{}.WithOptions(opts))
+}
+
+// CanonicalRequestComponentsWithConfig is the lowest level primitive of this
+// method; you should use CanonicalRequest or CanonicalRequestComponents
+// instead.
+func CanonicalRequestComponentsWithConfig(method, resource string, params url.Values, conf CanonicalRequestConfig) string {
 	b := &strings.Builder{}
 	b.WriteString(method)
 	b.WriteString(";")
@@ -24,8 +63,9 @@ func CanonicalRequestComponents(method, resource string, params url.Values) stri
 	keys := make([]string, 0, len(q))
 	vals := make(map[string][]string)
 
+	excl := conf.ExcludedParams()
 	for k, v := range q {
-		if k != jwtParam {
+		if _, ok := excl[k]; !ok {
 			sort.Strings(v)
 			keys = append(keys, k)
 			vals[k] = v
